b7d362f8c4fa627571e0257a2ae08a60edaec1e4
Marco <gilles.marco06@gmail.com>
2022-03-11 10:29:12 +0100

    Remove random AI and add input for each play

--- main.py
+++ main.py
@@ -32,28 +32,17 @@ while len(list(filter(lambda j: j.score >= 32, plateau.joueurs))) <= 0:
     print(plateau.joueurs)
 
     joueur = plateau.joueurs[numero_tour%2]
+    string_pair = "pair" if joueur.pair else "impair"
+    print(f"C'est au joueur {string_pair} de jouer.")
 
-    if (numero_tour%2 == 1):
-        # L'IA
-        ### TEMPORARY FOR TESTING, TO REMOVE WHEN DONE IMPLEMENTING IA
-        
-        # Choisir un trou pair random
-        # Choisir la couleur de graine rouge ou bleu
-        numero_trou = random.randrange(1, 16, 2)
-        couleur = "R" if random.randrange(1, 3) == 1 else "B"
-        action = f"{numero_trou}{couleur}"
-        while not plateau.valid_action(action, joueur):
-            numero_trou = random.randrange(1, 16, 2)
-            couleur = "R" if random.randrange(1, 3) == 1 else "B"
-            action = f"{numero_trou}{couleur}"
-        print(f"{numero_trou}{couleur}")
-    else:
-        # ACTION À JOUER DANS LA CONSOLE
+    # PREDICTION
+    
+
+    action = input("Indiquez l'action à jouer : ")
+    while not plateau.valid_action(action, joueur):
         action = input("Indiquez l'action à jouer : ")
-        while not plateau.valid_action(action, joueur):
-            action = input("Indiquez l'action à jouer : ")
 
-    # Donne l'action à réaliser au plateau
+    # Appliquer l'action à jouer
     plateau.play_action(action, plateau.joueurs[numero_tour%2])
 
     # Incrementation du tour
